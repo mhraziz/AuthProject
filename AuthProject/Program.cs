da855b8dd2bd7030180175453a21ce136cf3fbfb
Aziz <132378081+mhraziz@users.noreply.github.com>
2023-10-16 19:17:36 +0500

    Add project files.

---
+++ AuthProject/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace AuthProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(option =>
+                {
+                    option.LoginPath = "/Access/Login";
+                    option.ExpireTimeSpan=TimeSpan.FromMinutes(20);
+                });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Access}/{action=Login}/{id?}");
+
+            app.Run();
+        }
+    }
+}
